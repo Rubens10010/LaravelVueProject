f343a4d9ebd90781d5c7548046bf6f43bfa911a3
Rubens10010 <rhualla@unsa.edu.pe>
2020-01-26 18:56:50 -0500

    Notifications, progress bar, validation, events

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,18 +7,68 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
+import {Form, HasError, AlertError } from 'vform';
 
+ // -- Sweet alert --
+ import Swal from 'sweetalert2'
+ window.Swal = Swal;
+
+ const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+
+window.Toast = Toast;
+
+ // vform
+window.Form = Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+// vue-router
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
+// -- Progress Bar --
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    filledColor: 'red',
+    height: '3px'
+})
+
 const routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default},
-    { path: '/profile', component: require('./components/Profile.vue').default}
+    { path: '/users', component: require('./components/Users.vue').default},    { path: '/profile', component: require('./components/Profile.vue').default}
 ];
 
 const router = new VueRouter({
+    mode: 'history',
     routes: routes
 });
+
+// global filter for vue in components
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate', function(created){
+    return moment(created).format('MMMM Do YYYY');
+});
+
+// Custom events
+let Fire = new Vue();
+window.Fire = Fire;
+
 
 /**
  * The following block of code may be used to automatically register your
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -174,7 +174,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt blue"></i>
               <p>
                 Dashboard
               </p>
@@ -182,8 +182,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-cog"></i>
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Managment
                 <i class="right fas fa-angle-left"></i>
@@ -191,10 +191,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" class="nav-link">
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
@@ -207,7 +207,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user orange"></i>
               <p>
                 Profile
               </p>
@@ -215,12 +215,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-sign-out-alt"></i>
-              <p>
-                Logout
-              </p>
+            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-sign-out-alt red"></i>
+                <p>
+                  {{ __('Logout') }}
+                </p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
 
         </ul>
@@ -237,6 +241,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="content">
       <div class="container-fluid">
         <router-view></router-view>   <!-- carga un componente vue aca -->
+        <vue-progress-bar></vue-progress-bar>
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
